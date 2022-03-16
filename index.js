fc400e108433a5108d2eb236a2506fe98dbebb58
AlexMinez <alex_minihan@outlook.com>
2022-03-16 16:41:39 +1100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+function writeCards(cardNames,eventName){
+    const messages = []
+    for (let i = 0; i<cardNames.length; i++){
+       const message = `Thank you, ${cardNames[i]}, for the wonderful ${eventName} gift!`;
+       messages.push(message)
+    } return messages
+}
+
+
+
+function countDown(){
+
+    for (let i = 0; i <= 10; i++ ){
+        console.log(i)
+    }
+
+}
+console.log(writeCards())
